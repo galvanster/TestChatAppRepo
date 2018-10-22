4e9442ab6df1799f3088ee6b8e24d0abcb719063
Carlos Galvan <LISSAAB@capitol.local>
2018-10-22 14:01:29 -0500

    code done

---
+++ TestChatApp/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace TestChatApp
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(SendData data)
+        {
+            Clients.All.newMessage(data.name + ": " + data.message);
+        }
+    }
+
+    public class SendData
+    {   
+       public string message { get; set; }
+       public string name { get; set; }
+    }
+}
